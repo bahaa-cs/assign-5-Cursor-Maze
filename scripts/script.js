7607d5ef1aeb385b18f6b1da1969fe62afc76c6b
bahaaCs <bahaaaboukhalil@gmail.com>
2024-10-27 12:33:45 +0200

    Adding Event Listener on start and end

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 
 function testing(){
-    console.log("went well")
+    var end = document.getElementById("end")
+    end.addEventListener('mouseover',function(){alert("You Win")})
 }
 
 var start = document.getElementById("start")
 start.addEventListener('mouseover',testing)
+
